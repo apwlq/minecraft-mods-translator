3c5fa3fe3a82e3a04953a19fbe55f2c7817a6b18
MakerZip <makerzip@hanmail.net>
2023-01-18 22:04:49 +0900

    그냥 다운 받아서 쓰세유

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+from googletrans import Translator
+
+# 파일 경로는 ./en_us.json
+with open("./en_us.json", "r") as json_file:
+    data = json.load(json_file)
+
+en_us = []
+ko_kr = []
+
+imsi = ""
+for a in data:
+  for b in data[a]:
+    imsi = imsi + b
+  en_us.append(imsi)
+  imsi = ""
+
+for en in en_us:
+  # src='en' 부분이 원문, dest='ko'부분이 번역문, src 부분을 바꾸거나 지우면 그 언어로 가능.
+  ko = Translator().translate(en, src='en', dest='ko').text
+  ko_kr.append(ko)
+
+i = 0
+for key in data:
+  data[key] = ko_kr[i]
+  i = i + 1
+done = json.dumps(data, indent="\t", ensure_ascii=False)
+# print(done)
+
+# 파일 경로는 ./ko_kr.json
+with open("./ko_kr.json", "w") as file:
+    file.write(done)
+print("\n\ndone.")
